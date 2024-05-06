d48b55115c89c02a9538cfdcb0c32ae3025717d8
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2024-05-06 16:11:01 +0200

    Update OnlyOffice v6 and v7

--- install-onlyoffice.sh
+++ install-onlyoffice.sh
@@ -32,8 +32,8 @@ main () {
 	install_version v2b d9da72fd
 	install_version v4 6ebc6938
 	install_version v5 88a356f0
-	install_version v6 abd8a309
-	install_version v7 9d8b914a
+	install_version v6 646739b6
+	install_version v7 6aeed244
 	install_x2t v7.3+1 ab0c05b0e4c81071acea83f0c6a8e75f5870c360ec4abc4af09105dd9b52264af9711ec0b7020e87095193ac9b6e20305e446f2321a541f743626a598e5318c1
 
 	rm -rf "$BUILDS_DIR"
