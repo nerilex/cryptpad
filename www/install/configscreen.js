6b08f9d255981521faea24e429247c5bae762637
zuzanna-maria <zuzanna.ludzik@gmail.com>
2024-05-23 14:48:45 +0100

    screen

--- www/install/configscreen.js
+++ www/install/configscreen.js
@@ -18,6 +18,7 @@ define([
     '/api/instance',
     '/lib/datepicker/flatpickr.js',
     '/admin/customize.js',
+    '/common/loading.js',
 
 
 
@@ -45,26 +46,101 @@ define([
     ApiConfig,
     Instance,
     Flatpickr,
-    Customize
+    Customize, 
+    Loading
 ) {
 
 var AppConfigScreen = {}
 
+var LOADING = 'cp-loading';
 
-AppConfigScreen.addConfigScreen = function (content) {
+    // UI.addLoadingScreen = function (config) {
+    //     config = config || {};
+    //     var loadingText = config.loadingText;
+    //     var todo = function () {
+    //         var $loading = $('#' + LOADING);
+    //         // Show the loading screen
+    //         $loading.css('display', '');
+    //         $loading.removeClass('cp-loading-hidden');
+    //         $loading.removeClass('cp-loading-transparent');
+    //         if (config.newProgress) {
+    //             var progress = h('div.cp-loading-progress', [
+    //                 h('p.cp-loading-progress-list'),
+    //                 h('p.cp-loading-progress-container')
+    //             ]);
+    //             $loading.find('.cp-loading-spinner-container').after(progress);
+    //         }
+    //         if (!$loading.find('.cp-loading-progress').length) {
+    //             // Add spinner
+    //             $('.cp-loading-spinner-container').show();
+    //         }
+    //         // Add loading text
+    //         if (loadingText) {
+    //             $('#' + LOADING).find('#cp-loading-message').show().text(loadingText);
+    //         } else {
+    //             $('#' + LOADING).find('#cp-loading-message').hide().text('');
+    //         }
+    //     };
+    //     if ($('#' + LOADING).length) {
+    //         todo();
+    //     } else {
+    //         Loading();
+    //         todo();
+    //     }
+
+    //     $('html').toggleClass('cp-loading-noscroll', true);
+    //     // Remove the inner placeholder (iframe)
+    //     $('#placeholder').remove();
+    // };
+
+
+AppConfigScreen.addConfigScreen = function () {
         var LOADING = 'cp-loading';
+        config = config || {};
+        var loadingText = config.loadingText;
+        var todo = function () {
+            var $loading = $('#' + LOADING);
+            // Show the loading screen
+            $loading.css('display', '');
+            $loading.removeClass('cp-loading-hidden');
+            $loading.removeClass('cp-loading-transparent');
 
+        };
         if ($('#' + LOADING).length) {
+            todo();
         } else {
-            // var form = Customize.disableApps();
-            var elem = document.createElement('div');
-            elem.setAttribute('id', 'cp-loading');
-
-            let frame = h('div.configscreen',  {style: 'width: 70%; height: 75%; background-color: white'}, content)
-
-            elem.append(frame)
-
+            Loading();
+            todo();
         }
+
+        $('html').toggleClass('cp-loading-noscroll', true);
+        // Remove the inner placeholder (iframe)
+        $('#placeholder').remove();
+
+    //     if ($('#' + LOADING).length) {
+    //     } else {
+    //         // var form = Customize.disableApps();
+    //         var elem = document.createElement('div');
+    //         elem.setAttribute('id', 'cp-loading');
+
+    //         let frame = h('div.configscreen',  {style: 'width: 70%; height: 75%; background-color: white'}, content)
+
+    //         elem.append(frame)
+
+    //         return function () {
+    //             built = true;
+    //             var intr;
+    //             var append = function () {
+    //                 if (!document.body) { return; }
+    //                 clearInterval(intr);
+    //                 document.body.appendChild(elem);
+    //             };
+    //             intr = setInterval(append, 100);
+    //             append();
+    // };
+            
+
+    //     }
 
     };
 
